1604e0b482985264e57003dd7e45afea5f5bb641
John Sundh <john.sundh@scilifelab.se>
2024-01-14 05:52:48 +0100

    Update sintax parsing script

--- workflow/scripts/sintax_tsv.py
+++ workflow/scripts/sintax_tsv.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 ##### Program description #######
 #
-# Title: Subsetting Insect Biome ASVs 
+# Title: Parse SINTAX output
 #
 # Author(s): Lokeshwaran Manoharan
 #
 #
 #
-# Description: The ASVs that are supposed to be of particular length (418 ± nx3 and between 403 and 418), possibly does not contain any stop codon in the right reading frame. 
+# Description: Parse the SINTAX output and create a tsv file with taxonomy
 #  
 # List of subroutines: 
 #
@@ -15,12 +15,11 @@
 #
 # Overall procedure: using functions and dictionaries in python
 #
-# Usage: create_q2_md_lanes.py <sample_info_file> <location_of_trimmed_files> <output_ss_file>
+# Usage: sintax_tsv.py -i <sintax output> -o <sintax tsv output>
 ##################################
 
 import re
 import sys
-import copy
 import argparse
 
 usage = '''This program takes the output of SINTAX and creates readable taxonomy file'''
@@ -46,6 +45,9 @@ parser.add_argument(
     default=sys.stdout
     )
 
+parser.add_argument("-r", "--ranks", nargs="+", help="List of ranks to include in the output", 
+                    default=["kingdom", "phylum", "class", "order", "family", "genus", "species"])
+
 args = parser.parse_args()
 
 def complete_list(some_list, target_len):
@@ -53,25 +55,27 @@ def complete_list(some_list, target_len):
 
 p0 = re.compile('>')
 p1 = re.compile(' ')
-p2 = re.compile('\,')
+p2 = re.compile(',')
 p3 = re.compile('\t')
-p4 = re.compile('\;')
+p4 = re.compile(';')
 
-print('ASV', 'Kingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species', 'BOLD_bin', sep = '\t', file = args.outfile)
+header = ["ASV"]+args.ranks
+
+print("\t".join(header), sep = '', file = args.outfile)
 
 for line in args.infile:
     line = line.rstrip('\n')
     tmp_list = re.split(p3, line)
     if tmp_list[3] != '':
-        annot = re.sub('.\:', '', tmp_list[3])
+        annot = re.sub('[a-z]:', '', tmp_list[3])
         tmp_list1 = re.split(p2, annot)
-        if len(tmp_list1) != 8:
-            tmp_list1 = complete_list(tmp_list1, 8)
+        if len(tmp_list1) != len(args.ranks):
+            tmp_list1 = complete_list(tmp_list1, len(args.ranks))
             print(tmp_list[0], '\t'.join(tmp_list1), sep = '\t', file = args.outfile)
         else:
             print(tmp_list[0], '\t'.join(tmp_list1), sep = '\t', file = args.outfile)
     else:
-        print(tmp_list[0], '\t'.join(['unclassified'] * 8), sep = '\t', file = args.outfile)
+        print(tmp_list[0], '\t'.join(['unclassified'] * len(args.ranks)), sep = '\t', file = args.outfile)
 
 args.outfile.close()
 args.infile.close()
