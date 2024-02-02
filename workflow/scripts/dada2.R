73487686cebcb3a84abd8e017441949e2d05bfeb
John Sundh <john.sundh@scilifelab.se>
2024-02-03 00:08:38 +0100

    Output unprocessed taxonomy

--- workflow/scripts/dada2.R
+++ workflow/scripts/dada2.R
@@ -6,35 +6,38 @@ taxLevels <- snakemake@params$taxLevels
 ref<- snakemake@input$ref
 threads <- snakemake@threads
 set.seed(snakemake@params$seed)
+minBoot <- snakemake@params$minBoot
 
-taxa <- assignTaxonomy(seq,ref, minBoot = 50, taxLevels = taxLevels, multithread = threads, verbose=TRUE, outputBootstraps = TRUE)
-tx <- data.frame(ASV_ID = names(seq), taxa, sequence = row.names(taxa$tax), row.names = names(seq))
-
+taxa <- assignTaxonomy(seq,ref, minBoot = minBoot, taxLevels = taxLevels, multithread = threads, verbose=TRUE, outputBootstraps = TRUE)
+tx <- data.frame(ASV_ID = names(seq), taxa, row.names = names(seq))
+colnames(tx) <- gsub("tax.","", colnames(tx))
 # (2) Set confidence to the bootstrap for the most specific taxon
 # extract columns with taxonomic values
-tax <- tx[ , grepl( "tax." , names( tx ) ) ]
+#tax <- tx[ , grepl( "tax." , names( tx ) ) ]
+colnames(tx) <- gsub("tax.","", colnames(tx))
+
 # find first occurrence of NA
-res <- max.col(is.na(tax), ties = "first")
+#res <- max.col(is.na(tax), ties = "first")
 # correct if no NA is present in column to NA
-if(any(res == 1)) is.na(res) <- (res == 1) & !is.na(tax[[1]])
+#if(any(res == 1)) is.na(res) <- (res == 1) & !is.na(tax[[1]])
 # find index of last entry before NA, which is the bootstrap value
-res <- res-1
+#res <- res-1
 # if NA choose last entry
-res[is.na(res)] <- ncol(tax)
+#res[is.na(res)] <- ncol(tax)
 # extract bootstrap values
-boot <- tx[ , grepl( "boot." , names( tx ) ) ]
-boot$last_tax <- res
-valid_boot <- apply(boot,1,function(x) x[x[length(x)]][1]/100 )
+#boot <- tx[ , grepl( "boot." , names( tx ) ) ]
+#boot$last_tax <- res
+#valid_boot <- apply(boot,1,function(x) x[x[length(x)]][1]/100 )
 # replace missing bootstrap values (NA) with 0
-valid_boot[is.na(valid_boot)] <- 0
+#valid_boot[is.na(valid_boot)] <- 0
 # add bootstrap values to column confidence
-tx$confidence <- valid_boot
+#tx$confidence <- valid_boot
 
 # (3) Reorder columns before writing to file
-expected_order <- c("ASV_ID",paste0("tax.",taxLevels),"confidence")
+expected_order <- c("ASV_ID",taxLevels)
 expected_order <- intersect(expected_order,colnames(tx))
 taxa_export <- subset(tx, select = expected_order)
-colnames(taxa_export) <- sub("tax.", "", colnames(taxa_export))
-rownames(taxa_export) <- names(seq)
+#colnames(taxa_export) <- sub("tax.", "", colnames(taxa_export))
+#rownames(taxa_export) <- names(seq)
 
 write.table(taxa_export, file = snakemake@output[[1]], sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE, na = '')
